e6a574821b55bd5919c2a58411185a83f9682a34
Naichuan Zhang <zhangnaichuan168@gmail.com>
2020-02-05 16:12:25 +0000

    Splash Activity Enlarge Anim Added

--- app/src/main/java/com/example/imageprocessor/activity/SplashActivity.java
+++ app/src/main/java/com/example/imageprocessor/activity/SplashActivity.java
@@ -3,14 +3,21 @@ package com.example.imageprocessor.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.example.imageprocessor.R;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private Animation animation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +25,26 @@ public class SplashActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
 
-        handler.sendMessageDelayed(new Message(), 2000);
+        animation = AnimationUtils.loadAnimation(this, R.anim.splash_enlarge);
+        imageView = findViewById(R.id.splash_image);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    imageView.startAnimation(animation);
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                redirect();
+            }
+        }).start();
     }
 
-    private Handler handler = new Handler() {
-
-        public void handleMessage(Message message) {
-            // TODO: Change Start to Main later ...
-            Intent intent = new Intent(SplashActivity.this, StartActivity.class);
-            SplashActivity.this.startActivity(intent);
-            finish();
-        }
-    };
+    private void redirect() {
+        Intent intent = new Intent(this, StartActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }
